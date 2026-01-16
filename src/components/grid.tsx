2becac32b6720e53744e79d2fb3e838535caf552
Super Zombi <super.zombi.yt@gmail.com>
2026-01-16 18:50:56 +0000

    fix tailwind dynamic styles

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -11,9 +11,13 @@ interface GridProps {
 
 export default function Grid({ itemW=380, gap=4, className="", children }: GridProps) {
     return (
-        <div className={
-            `grid grid-cols-1 gap-${gap} md:grid-cols-[repeat(auto-fill,minmax(${itemW}px,1fr))] ${className}`.trim()
-        }>
+        <div
+            className={`grid grid-cols-1 ${className}`.trim()}
+            style={{
+                gap: `${gap * 4}px`,
+                gridTemplateColumns: `repeat(auto-fill, minmax(${itemW}px, 1fr))`,
+            }}
+        >
             {children}
         </div>
     );
